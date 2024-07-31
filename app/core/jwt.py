e83b2b4d0bcfa923f3d35f99b08218e74fb0ffac
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-31 18:50:31 +0300

    Implemented password hashing and jwt token generation

---
+++ app/core/jwt.py
@@ -0,0 +1,27 @@
+# app/core/jwt.py
+from datetime import datetime, timedelta,timezone
+import os
+from typing import Optional
+from dotenv import load_dotenv
+import jwt
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for application")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
