6026f3726db37193bd4610a3ea8b9bb6f00cb824
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-08-05 21:41:45 +0300

    Moved database url to .env

--- app/database.py
+++ app/database.py
@@ -3,8 +3,12 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
