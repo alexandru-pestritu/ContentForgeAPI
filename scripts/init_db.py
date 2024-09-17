de45dbf2f695825b160d6f9f6f3e9c50e6a3ba36
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-09-17 14:44:59 +0300

    Added initial migrations

--- scripts/init_db.py
+++
@@ -1,8 +0,0 @@
-from app.database import Base, engine
-from app.models.store import Store
-from app.models.user import User
-from app.models.product import Product
-from app.models.article import Article
-from app.models.prompt import Prompt
-
-Base.metadata.create_all(bind=engine)
